beba01f7c88d592435580616e8ac00ccfc247b8b
Youssoupheguisse <84451543+Youssoupheguisse@users.noreply.github.com>
2021-06-17 12:44:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-# teste de parite des nombre entier naturiel
-n = int(input("entre un nombre "))
-if n%2==0:
-    print("nombre [n] est paire")
-else:
-    print("nombre [n] est impaire")
+# Le factoriel d"un nombre
+n= int(input("entre un nombre "))
+while n<0:
+    n=int(input("entre un nombre "))
+p=1
+i=0
+if n>=0:
+    for i in range(1,n+1):
+        p=p*i
+    print("le factoriel  est",p)
